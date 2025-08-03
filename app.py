fead8db6af6d399281c55755481d4dd0f0c62220
Devasshish <devashishmac13@gmail.com>
2025-08-03 14:30:04 +0530

    Expose app for gunicorn

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+# app.py
+
 from app import create_app
 
-app = create_app()
+app = create_app()  # 👈 make sure this line is present at the top level
 
 if __name__ == '__main__':
     app.run(debug=True)
